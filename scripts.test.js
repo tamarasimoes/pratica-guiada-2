14907bb906ac9bcc351b9649c33da45a46aa103b
Tamara Simões <tamara.backups@gmail.com>
2024-11-09 10:08:24 -0300

    Provoca erro

--- scripts.test.js
+++ scripts.test.js
@@ -13,7 +13,7 @@ global.fetch = jest.fn(() =>
       Promise.resolve([
         { id: 1, name: "Item A", value: 10 },
         { id: 2, name: "Item B", value: 20 },
-        { id: 3, name: "Item A", value: 30 },
+        { id: 3, name: "Item A", value: 40 },
       ]),
   })
 );
